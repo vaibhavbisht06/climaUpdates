ea7a8896600cd2392fff758920d66e563409bb50
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2019-10-17 21:32:44 +0530

    GPS Tappings

--- Climate Update/Climate Update/Controller/WeatherViewController.swift
+++ Climate Update/Climate Update/Controller/WeatherViewController.swift
@@ -36,6 +36,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     
         
         
@@ -80,11 +81,16 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the didUpdateLocations method here:
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
     
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        <#code#>
+    }
     
     
 
