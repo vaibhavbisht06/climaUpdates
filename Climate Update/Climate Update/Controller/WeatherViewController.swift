bd4fabf25c655c9d1c53b2e9a351c04cd4ecfbaa
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2019-10-19 22:02:18 +0530

    Weather URl added

--- Climate Update/Climate Update/Controller/WeatherViewController.swift
+++ Climate Update/Climate Update/Controller/WeatherViewController.swift
@@ -13,7 +13,7 @@ import CoreLocation
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
-    let WEATHER_URL = " "
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "e72ca729af228beabd5d20e3b7749713"
     
 
